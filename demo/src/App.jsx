4b43a3de102e9f924a3cd8aa85ccfad17a26e672
Brasero <brandonricci4@gmail.com>
2025-02-03 16:50:34 +0100

    presentation de la gestion des évènements

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,17 +1,26 @@
 import './App.css'
-import Counter from "./component/Counter.jsx";
 
 
 
 function App() {
-  
+ 
+ const handleClick = (message) => {
+  alert(message)
+ }
+ 
+ const handleSubmit = (event) => {
+  event.preventDefault()
+  console.log("hello")
+ }
   
 
   return (
-    <>
-      <Counter />
-      <Counter step={5} />
-    </>
+   <>
+    <form onSubmit={handleSubmit}>
+     <button type={"submit"} role={"button"}>Envoyer</button>
+    </form>
+    <button onClick={() => handleClick("Mon message")}>Click me</button>
+   </>
   )
 }
 
