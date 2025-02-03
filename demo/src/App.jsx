abc88f445f8136cd129534ae7c3397d4d87113a9
Brasero <brandonricci4@gmail.com>
2025-02-03 15:28:19 +0100

    proposition de correction

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,19 +1,18 @@
 import './App.css'
-import Messages from "./component/Messages.jsx";
+import Students from "./component/Students.jsx";
 
 function App() {
   
-  const MESSAGES = [
-    { message : "React JS" },
-    { message : "React Native" },
-    { message : "Angular" },
-    { message : "Symfony" },
-    { message : "MongoDB" },
+  const students = [
+    { notes: [12, 11, 10], name: "Alan" },
+    { notes: [18, 10, 19], name: "Alice" },
+    { notes: [10, 9, 11], name: "Bernard" },
+    { notes: [11, 17, 19], name: "Sophie" },
   ];
 
   return (
     <>
-      <Messages messages={MESSAGES} />
+    <Students students={students} />
     </>
   )
 }
