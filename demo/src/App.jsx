fe123772ece1a9dc9e5477979190cc3246447194
Brasero <brandonricci4@gmail.com>
2025-02-03 15:48:00 +0100

    presentation de la gestion des states (états)

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,18 +1,15 @@
 import './App.css'
-import Students from "./component/Students.jsx";
+import TestState from "./component/TestState.jsx";
+import TestStateFunc from "./component/TestStateFunc.jsx";
 
 function App() {
   
-  const students = [
-    { notes: [12, 11, 10], name: "Alan" },
-    { notes: [18, 10, 19], name: "Alice" },
-    { notes: [10, 9, 11], name: "Bernard" },
-    { notes: [11, 17, 19], name: "Sophie" },
-  ];
+  
 
   return (
     <>
-    <Students students={students} />
+      <TestState />
+     <TestStateFunc />
     </>
   )
 }
