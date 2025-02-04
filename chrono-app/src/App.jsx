6cab622161a8b116cbd2cf2f13e8161676fe5e5e
Brasero <brandonricci4@gmail.com>
2025-02-04 15:13:26 +0100

    propostion de correction

--- chrono-app/src/App.jsx
+++ chrono-app/src/App.jsx
@@ -1,24 +1,12 @@
 import './App.css'
-import Chrono from "./component/Chrono.jsx";
-import EffectTest from "./component/EffectTest.jsx";
-import {useState} from "react";
+import ClickCount from "./component/ClickCount.jsx";
 
 function App() {
 
-  const [show, setShow] = useState(true)
-  
-  const handleClick = () => {
-    setShow(!show)
-  }
   
   return (
     <>
-      <button onClick={handleClick}>{show ? "Cacher": "Montrer"}</button>
-      {
-        show && <EffectTest />
-      }
-    <Chrono />
-    <Chrono isCyclique={true} />
+    <ClickCount />
     </>
   )
 }
